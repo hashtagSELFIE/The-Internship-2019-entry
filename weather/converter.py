d761150d8eee65854b2a257e526ca1367cc2ddd3
Chetsadaporn Traivinidsreesuk <35063331+hashtagSELFIE@users.noreply.github.com>
2019-01-29 17:23:06 +0700

    Finisihed weather project

--- weather/converter.py
+++ weather/converter.py
@@ -19,24 +19,40 @@ def newformat():
 Read the 'weather.xml' file then convert it using xmltodict libary, then export it
 in to readable JSON file
     """
-    print("Welcome to XML to JSON parser.\n\n Enter for demo mode(Read the preset example file)\n\n\
-Enter 'SUDO' for your custom xml context file\nMake sure you put custom file in THIS exact same\
-folder as this Python file directory!\n\n")
-    filename = 'import.xml'
+    print("Welcome to XML to JSON parser.\n\nEnter for demo mode(Read the preset example file)\n\n\
+Enter 'SUDO' for your custom xml context file\nMake sure you put your custom file in THIS exact same\
+folder as this Python file directory!\n")
+    filename = 'weather.xml'
     response = input()
+    realfilename = ''
     os.system('cls' if os.name == 'nt' else 'clear')
     if response in ("SUDO", "'SUDO'"):
         while 1:
             os.system('cls' if os.name == 'nt' else 'clear')
             print("Please enter EXACT file name down below (Example: export.xml)\n")
             filename = input()
-
+            print("\nPress enter to confirm\nType in 'EDIT' to re-enter file name.\n")
+            answer = input()
+            if answer in ("EDIT", "'EDIT'"):
+                continue
+            else:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print("Custom file accepted!")
+                break
     with open(filename, 'rb') as xml_file:
         raw_json = json.loads(json.dumps(xmltodict.parse(xml_file)))
     print("Conversion done!")
-    with open('export.json', 'w') as export:
-        json.dump(raw_json, export, indent=4)
-    print("Export done!\n\nPress Enter to exit this program")
+    for i in filename:
+        if i == ".":
+            break
+        realfilename += i
+    with open(realfilename + '_parsed.json', 'w') as export:
+        if realfilename == 'weather':
+            json.dump(raw_json["current"], export, indent=2)
+        else:
+            json.dump(raw_json, export, indent=2)
+    print("File created as", realfilename + '_parsed.json')
+    print("Export done!\n\nPress Enter to exit this program.")
     input()
 
 
